58c863050a3002a2e6bb7159f3c3da5e0ac052f3
Simon Constans <simonc@linux.com>
2016-11-08 01:18:02 +0100

    Add library with some tests :sunflower:

--- sharepear-gallery-picture-size.js
+++ sharepear-gallery-picture-size.js
@@ -1,7 +1,86 @@
+import LinearPartitioning from 'linear-partitioning'
+
+/**
+ * Get Perfect Picture Height
+ */
+function getPerfectHeight (width) {
+  return Math.round(width / 2 * 100) / 100
+}
+
+/**
+ * Set for each picture the ratio
+ */
+function setPicturesRatio (pictures) {
+  for (let picture of pictures) {
+    picture.ratio = picture.width / picture.height
+  }
+
+  return pictures
+}
+
+/**
+ * Get summed width
+ */
+function getSummedWidth (pictures, perfectHeight) {
+  let summedWidth = 0
+
+  for (let picture of pictures) {
+    summedWidth += picture.ratio * perfectHeight
+  }
+
+  return summedWidth
+}
+
+/**
+ * Get partitions
+ */
+function getPartitions (pictures, summedWidth, galleryWidth) {
+  const rows = Math.round(summedWidth / galleryWidth)
+
+  const weights = pictures.map((picture) => {
+    return parseInt(picture.ratio * 100)
+  })
+
+  return LinearPartitioning(weights, rows)
+}
+
+/**
+ * Get pictures size
+ */
 function getPicturesSize (pictures, galleryWidth) {
+  let picturesWithSize = JSON.parse(JSON.stringify(pictures)) // clone pictures
+  picturesWithSize = setPicturesRatio(picturesWithSize)
+
+  const perfectHeight = getPerfectHeight(galleryWidth)
+  const summedWidth = getSummedWidth(picturesWithSize, perfectHeight)
+  const partitions = getPartitions(picturesWithSize, summedWidth, galleryWidth)
+
+  let indexStart = 0
+  partitions.forEach((row) => {
+    let indexEnd = indexStart + row.length
+    const summedRatios = picturesWithSize.filter((picture, index) => {
+      return index >= indexStart && index < indexEnd
+    }).reduce((sum, picture) => {
+      sum += picture.ratio
+      return sum
+    }, 0)
+
+    for (let i = indexStart; i < indexEnd; i++) {
+      var picture = picturesWithSize[i]
+      picture.calculatedWidth =
+        Math.round(galleryWidth / summedRatios * picture.ratio * 100) / 100
+      picture.calculatedHeight =
+        Math.round(galleryWidth / summedRatios * 100) / 100
+    }
+
+    indexStart = indexEnd
+  })
+
   return {
-    pictures: pictures,
-    galleryWidth: galleryWidth
+    pictures: picturesWithSize,
+    galleryWidth: galleryWidth,
+    perfectHeight: perfectHeight,
+    partitions: partitions
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -2,10 +2,54 @@
 import assert from 'assert'
 import getPicturesSize from '../sharepear-gallery-picture-size'
 
+let galleryWidth = 1024
+let picture = [
+  { width: 2048, height: 1024 }
+]
+
 describe('Sharepear Gallery Picture Size', () => {
-  describe('getPicturesSize', () => {
-    it('should return an array', () => {
-      assert.deepEqual(getPicturesSize(['test'], 0).pictures, ['test'])
+  describe('getPicturesSize pictures', () => {
+    it('should return an object with pictures key', () => {
+      assert.ok(getPicturesSize(picture, galleryWidth).pictures)
+    })
+  })
+
+  describe('getPicturesSize galleryWidth', () => {
+    it('should return an object with galleryWidth key', () => {
+      let gallery = getPicturesSize(picture, galleryWidth)
+      assert.equal(gallery.galleryWidth, galleryWidth)
+    })
+  })
+
+  describe('getPicturesSize perfectHeight', () => {
+    let gallery = getPicturesSize(picture, 1000 / 3)
+
+    it('should return an object with perfectHeight key', () => {
+      assert.ok(gallery.perfectHeight)
+    })
+
+    it('should return perfectHeight with width / 2 arround', () => {
+      assert.equal(gallery.perfectHeight, 166.67)
+    })
+  })
+
+  describe('getPicturesSize pictures ratio', () => {
+    const picturesRatio = [
+      { width: 1, height: 1, ratio: 1 },
+      { width: 2, height: 3, ratio: 0.6666666666666666 },
+      { width: 3, height: 2, ratio: 1.5 },
+      { width: 3, height: 4, ratio: 0.75 },
+      { width: 5, height: 3, ratio: 1.6666666666666667 },
+      { width: 5, height: 4, ratio: 1.25 },
+      { width: 8, height: 5, ratio: 1.6 },
+      { width: 16, height: 9, ratio: 1.7777777777777777 }
+    ]
+    const gallery = getPicturesSize(picturesRatio, galleryWidth)
+
+    it('should return an object with picture.ratio key', () => {
+      for (let i = 0; i < picturesRatio.length; i++) {
+        assert.equal(picturesRatio[i].ratio, gallery.pictures[i].ratio)
+      }
     })
   })
 })
